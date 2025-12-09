8ce7fcae7f970146f452270ff83e10064d5f5a12
skeler <168662493+Azyrn@users.noreply.github.com>
2025-12-09 23:52:31 +0100

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,5 +1,5 @@
 #!/system/bin/sh
 MODPATH=$1
-ui_print "Installing DeepDoze Enforcer v1.1"
+ui_print "Installing DeepDoze Enforcer v1.3"
 set_perm_recursive $MODPATH/system/bin 0 0 0755 0755
 ui_print "Installation complete"
